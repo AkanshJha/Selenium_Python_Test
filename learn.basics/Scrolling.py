eef2487f97138c0a4f67b125a9be023cc9689b73
Akansh Jha <akansh.jha@gmail.com>
2020-04-28 15:34:20 +0530

    change in Scrolling.py code

--- learn.basics/Scrolling.py
+++ learn.basics/Scrolling.py
@@ -2,7 +2,7 @@ import time
 
 from selenium import webdriver
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path="../drivers/chromedriver.exe")
 driver.implicitly_wait(5)
 driver.maximize_window()
 
